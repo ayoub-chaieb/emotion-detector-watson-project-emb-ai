e7dd2604f8c81be07f58a02b7838918c95b605ff
ayoub-chaieb <ayoubchaieb75@gmail.com>
2025-09-14 14:18:44 -0400

    Added necessary code for emotion detection function

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,17 +1,18 @@
-# Import the Flask class from the flask module
-from flask import Flask
+import requests  # Import the requests library to handle HTTP requests
 import json
 
-# Create an instance of the Flask class, passing in the name of the current module
-app = Flask(__name__)
-
-
-url='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-headers={"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-input_json={ "raw_document": { "text": text_to_analyse } }
-text_to_analyse=""
-
 def emotion_detector(text_to_analyse):
-    
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # URL of the Emotion Predict service of the Watson NLP Library.
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+
+    # Constructing the request payload in the expected format
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    # Custom header specifying the model ID for the Emotion Predict service
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
+    # Sending a POST request to the Emotion Predict API
+    response = requests.post(url, json=myobj, headers=header)
+
+    # Returning a text attribute of the response object
+    return response.text
